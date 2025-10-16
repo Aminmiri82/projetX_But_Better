5016f1e0a8b705e28f7a755051da9f08c3895ff1
YM82 <miri.amin@ymail.com>
2025-10-16 15:21:56 +0200

    changed the secret number to 42 and changed styling

--- devinette.py
+++ devinette.py
@@ -1,8 +1,8 @@
 print("Voici la devinette")
 
-nombre_secret = 23
+nombre_secret = 42
 
-nombre_utilisateur = int(input("Taper un nombre entre 0 et 100"))
+nombre_utilisateur = int(input("Taper un nombre entre 0 et 100 : "))
 
 if nombre_utilisateur == nombre_secret:
     print("Gagné")
